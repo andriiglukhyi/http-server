63547936149ad9a8127ab264059d33ab9eb3ece4
andriiglukhyi <andrigluxii@gmail.com>
2018-04-08 17:19:59 -0700

    fixed stuff

--- src/server.py
+++ src/server.py
@@ -1,23 +1,30 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 import json
-import sys
+# import sys
 from cowpy import cow
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
+        print(parsed_path)
+        # parsed_path = Parsed_Path(path='/cat/books', query='name='Frank Herbert'&genre='SciFi'&....')
         parsed_qs = parse_qs(parsed_path.query)
+
         if parsed_path.path == '/':
+            # Set Headers
             self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
             self.end_headers()
+
+            # Set Body
             self.wfile.write(b'''
                 <!DOCTYPE html>
                 <html>
                 <head>
                     <title> cowsay </title>
-                </head>
+                    </head>
                 <body>
                     <header>
                         <nav>
@@ -28,23 +35,22 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                     <header>
                     <main>
                     <p>Project description</p>
-                    <p>a socket client that sends a properly-formatted HTTP request, 
-                    and a server that parses a properly-formatted HTTP request 
+                    <p>a socket client that sends a properly-formatted HTTP request,
+                    and a server that parses a properly-formatted HTTP request
                     and returns a properly-formatted HTTP response</p>
                     </main>
                 </body>
                 </html>
                 ''')
-            return
-        
+
         elif parsed_path.path == '/cowsay':
             cheese = cow.Moose(tongue=True)
-            msg = cheese.milk("messege about stuff")
+            msg = cheese.milk("message about stuff")
 
             self.send_response(200)
             self.end_headers()
             self.wfile.write(msg.encode('utf8'))
-        
+
         elif parsed_path.path == '/cow':
             # import pdb; pdb.set_trace()
             try:
@@ -60,8 +66,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(msg.encode('utf8'))
-            return
-        
+
         else:
             self.send_response(404)
             self.end_headers()
@@ -69,21 +74,24 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         parsed_path = urlparse(self.path)
-        parsed_qs = parse_qs(parsed_path.query)
-        if parsed_path == '/cow':
+        # parsed_qs = parse_qs(parsed_path.query)
+        if parsed_path.path == '/cow':
             try:
-                cat = parsed_qs['msg'][0]
+                content_length = int(self.headers['Content-Length'])
+                body = json.loads(self.rfile.read(content_length).decode('utf8'))
+                # import pdb; pdb.set_trace()
             except KeyError:
                 self.send_response(400)
                 self.end_headers()
                 self.wfile.write(b'something is broken')
                 return
             cheese = cow.Moose(tongue=True)
-            msg = cheese.milk(cat)
+            msg = cheese.milk(body['msg'])
 
             self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.writeJSON({'content': msg})
+            self.wfile.write(json.dumps({'content': msg}).encode('utf8'))
             return
         else:
             self.send_response(404)
